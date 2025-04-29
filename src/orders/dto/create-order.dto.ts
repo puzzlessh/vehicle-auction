d718333445e81071ccd976a1871bf96bc7a0a674
Anton <anton.pak@cool-soft.ru>
2025-04-29 23:39:00 +0300

    PartialType useGlobalPipes(whitelist, forbidNonWhitelisted)

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,20 +1,56 @@
+import { IsBoolean, IsDate, IsNumber, IsString } from 'class-validator';
 export class CreateOrderDto {
+  @IsString()
   readonly creator: string;
+
+  @IsString()
   readonly customer: string;
+
+  @IsString()
   readonly model: string;
+
+  @IsString()
   readonly order: string;
+
+  @IsString({ each: true })
   readonly imageList: string[];
+
+  @IsString()
   readonly vin: string;
+
+  @IsNumber()
   readonly odometer: number;
+
+  @IsString()
   readonly vehiclePassport: string;
+
+  @IsString()
   readonly plateNumber: string;
+
+  @IsString()
   readonly bodyColor: string;
+
+  @IsString()
   readonly description: string;
+
+  @IsDate()
   readonly deadLine: Date;
+
+  @IsBoolean()
   readonly isEvacuationRequired: boolean;
+
+  @IsBoolean()
   readonly isPrepaymentAllowed: boolean;
+
+  @IsBoolean()
   readonly isAuctionStarted: boolean;
+
+  @IsString()
   readonly type: string;
+
+  @IsString()
   readonly status: string;
+
+  @IsDate()
   readonly createAt: Date;
 }
--- src/orders/dto/update-order.dto.ts
+++ src/orders/dto/update-order.dto.ts
@@ -1,20 +1,4 @@
-export class UpdateOrderDto {
-  readonly creator?: string;
-  readonly customer?: string;
-  readonly model?: string;
-  readonly order?: string;
-  readonly imageList?: string[];
-  readonly vin?: string;
-  readonly odometer?: number;
-  readonly vehiclePassport?: string;
-  readonly plateNumber?: string;
-  readonly bodyColor?: string;
-  readonly description?: string;
-  readonly deadLine?: Date;
-  readonly isEvacuationRequired?: boolean;
-  readonly isPrepaymentAllowed?: boolean;
-  readonly isAuctionStarted?: boolean;
-  readonly type?: string;
-  readonly status?: string;
-  readonly createAt?: Date;
-}
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateOrderDto } from './create-order.dto';
+
+export class UpdateOrderDto extends PartialType(CreateOrderDto) {}
