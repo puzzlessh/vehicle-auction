188a1867c887292eddf200d91363fbfd966ca97b
Anton <anton.pak@cool-soft.ru>
2025-04-30 23:49:10 +0300

    order.entity add orders.module

--- src/orders/entities/order.entity.ts
+++ src/orders/entities/order.entity.ts
@@ -7,6 +7,7 @@ export enum OrderStatus {
   COMPLETED = 'COMPLETED',
 }
 
+@Entity()
 export class Order {
   @PrimaryGeneratedColumn()
   id: number;
--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Order } from './entities/order.entity';
 
-@Module({ controllers: [OrdersController], providers: [OrdersService] })
+@Module({
+  imports: [TypeOrmModule.forFeature([Order])],
+  controllers: [OrdersController],
+  providers: [OrdersService],
+})
 export class OrdersModule {}
